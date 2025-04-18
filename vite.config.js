7ceed24b1dee53f51493c73f24c0eaf3a448f42e
imbimboluciano <luciano.imbimbo01@gmail.com>
2025-04-18 17:00:04 +0200

    Ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'portfolio';
+
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: true,  // Equivalent to 0.0.0.0
     port: 5173,
   },
+  base: `/${repoName}/`,
   plugins: [react()],
 })
